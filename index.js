e0767749166926217a150246f4fae6c0993499ea
tushar <tushark@uw.edu>
2020-04-20 11:49:11 -0700

    mostly done

--- index.js
+++ index.js
@@ -4,12 +4,9 @@
  * Section: CSE 154 AG
  * TA: Kyle Christian Roland
  *
- * -- your description of what this file does here --
  * Javascript file for creative project 2. This file handles header resizing,
  * makes the header invisble upon scrolling down, and makes it visible upon
  * scrolling up. It also handles event responses for a search bar.
- * Do not keep comments from this template in any work you submit (functions included under "Helper
- * functions" are an exception, you may keep the function names/comments of id/qs/qsa/gen)
  */
 "use strict";
 
@@ -17,29 +14,29 @@
 
   // global variable
   let scrollPosition = 0;
+  const SCROLL_TRIGGER = 100;
 
   /**
-   * Function to call when the window is loaded
+   * Initialize event handlers when the window is loaded
    */
   window.addEventListener("load", init);
 
   /**
-   * Adds event listeners for scrolling (for header resizing) and to the search bar
+   * Adds event listeners for scrolling (for header resizing, hidding and making it visible
+   * on upscroll) and to the search bar
    */
   function init() {
-    // THIS IS THE CODE THAT WILL BE EXECUTED ONCE THE WEBPAGE LOADS
     window.addEventListener('scroll', resizeHeader);
     window.addEventListener('scroll', hideHeader);
-    window.addEventListener('scroll', makeHeaderVisible);
+    window.addEventListener('scroll', showHeaderOnUpscroll);
+    window.addEventListener('scroll', removeSearchResults);
     id('search').addEventListener('input', showSearchResults);
   }
 
   /**
-   *  Resizes header and makes it invisible when the user scrolls past the top of the body
-   * @listens window:scroll
+   *  Resizes header when the user scrolls past the top of the body
    */
   function resizeHeader() {
-    const SCROLL_TRIGGER = 100;
     let header = qs('header');
     let newScrollPosition = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
     if (newScrollPosition > SCROLL_TRIGGER) {
@@ -50,11 +47,9 @@
   }
 
   /**
-   *  Resizes header and makes it invisible when the user scrolls past the top of the body
-   * @listens window:scroll
+   *  Hides header when the user scrolls past the top of the body
    */
   function hideHeader() {
-    const SCROLL_TRIGGER = 100;
     let header = qs('header');
     let newScrollPosition = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
     if (newScrollPosition > SCROLL_TRIGGER) {
@@ -66,10 +61,8 @@
 
   /**
    * Makes header visible when user scrolls up
-   * @listens window:scroll
    */
-  function makeHeaderVisible() {
-    const SCROLL_TRIGGER = 100;
+  function showHeaderOnUpscroll() {
     let header = qs('header');
     let newScrollPosition = Math.max(document.body.scrollTop, document.documentElement.scrollTop);
     if (newScrollPosition > SCROLL_TRIGGER && newScrollPosition < scrollPosition) {
@@ -80,6 +73,7 @@
 
   /**
    * Created a search drop when the user types a search query
+   * @param {event} e - The search input event to be handled
    */
   function showSearchResults(e) {
     let searchBox = id('search-results-box');
@@ -127,25 +121,14 @@
 
   }
 
-
   /**
-   * Make sure to always add a descriptive comment above
-   * every function detailing what it's purpose is
-   * Use JSDoc format with @param and @return.
+   * Removes search drop when the user scrolls
    */
-  function exampleFunction1() {
-    /* SOME CODE */
-  }
-
-  /**
-   * Make sure to always add a descriptive comment above
-   * every function detailing what it's purpose is
-   * @param {variabletype} someVariable This is a description of someVariable, including, perhaps, preconditions.
-   * @returns {returntype} A description of what this function is actually returning
-   */
-  function exampleFunction2(someVariable) {
-    /* SOME CODE */
-    return something;
+  function removeSearchResults() {
+    let searchBox = id('search-results-box');
+    if (searchBox) {
+      searchBox.remove();
+    }
   }
 
   /** ------------------------------ Helper Functions  ------------------------------ */
@@ -170,15 +153,6 @@
    */
   function qs(selector) {
     return document.querySelector(selector);
-  }
-
-  /**
-   * Returns the array of elements that match the given CSS selector.
-   * @param {string} selector - CSS query selector
-   * @returns {object[]} array of DOM objects matching the query.
-   */
-  function qsa(selector) {
-    return document.querySelectorAll(selector);
   }
 
   /**
